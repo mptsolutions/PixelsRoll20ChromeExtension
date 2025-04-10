cabd7efe8887d533c7dd391f06d14dbdbd872c5a
mptsolutions <miketurner4@gmail.com>
2025-04-09 20:14:21 -0400

    update to manifest v3

--- background.js
+++ background.js
@@ -1,16 +1,18 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function () {
+chrome.runtime.onInstalled.addListener(() => {
   // Storage example
   //chrome.storage.sync.set({color: '#3aa757'}, function() { console.log("The color is green."); });
 
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'app.roll20.net' }
-      })
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: 'app.roll20.net' }
+        })
       ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
+      actions: [new chrome.declarativeContent.ShowAction()]
     }]);
   });
 });

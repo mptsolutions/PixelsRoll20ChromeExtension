a8864a48488900fae487c17ee75f2376d5ffa060
mptsolutions <miketurner4@gmail.com>
2025-05-04 14:39:17 -0400

    rewrite of popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,329 +1,216 @@
 'use strict';
 
-class Message {
-  constructor(name, formula, advDisadvantage, sumRolls, source = 'storage') {
-    this.name = name;
-    this.formula = formula;
-    this.advDisadvantage = advDisadvantage;
-    this.sumRolls = sumRolls;
-    this.source = source;
-  }
+/* 
+ * Roll20Pixels Chrome Extension
+ * This file contains the code for the popup window of the extension.
+ */
 
-  static fromJSON(json) {
-    return new Message(json.name, json.formula, json.advDisadvantage, json.sumRolls, 'json');
-  }
 
-  static fromStorage(storage) {
-    return new Message(storage.name, storage.formula, storage.advDisadvantage, storage.sumRolls, 'storage');
-  }
+/* 
+ * Setting 'debug' to true will enable debug messages in the console
+ */
+const debug = true;
 
-  toJSON() {
-    return {
-      name: this.name,
-      formula: this.formula,
-      advDisadvantage: this.advDisadvantage,
-      sumRolls: this.sumRolls,
-      source: this.source
-    };
-  }
-
-  toStorage() {
-    return {
-      name: this.name,
-      formula: this.formula,
-      advDisadvantage: this.advDisadvantage,
-      sumRolls: this.sumRolls
-    };
-  }
-
-  toOption() {
-    return $('<option>', {
-      value: toCamelCase(this.name),
-      text: this.name,
-      'data-formula': this.formula,
-      'data-advdisadvantage': this.advDisadvantage,
-      'data-sumrolls': this.sumRolls,
-      'data-source': this.source
-    });
-  }
+/* 
+ * Helper function to create consistently formated debug messages.
+ */
+function logger(msg) { 
+    if (!debug) { return; }
+    const date = new Date();
+    const timestamp = date.toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    console.log(`[${timestamp}] ${msg}`);
 }
 
-function hookButton(name) {
-  $(`#${name}`).click(() => sendMessage({ action: name }));
-}
-
-function showDice(dice) {
-  const diceContainer = $('#diceList');
-  const blockDice = $('#blockdice');
-  console.log('Dice:', dice);
-  diceContainer.empty();
-  if (dice.length === 0) {
-    diceContainer.append($('<div>', { class: 'dice' }).text('No dice connected'));
-    updateDiceCount(0);
-  } else {
-    dice.forEach(die => {
-      const diceElement = $('<div>', { class: `dice ${toCamelCase(die.status)}`, 'data-name': die.name }).append(
-        $('<span>').text(die.name),
-        $('<span>').text(`Token: ${die.token}`),
-        $('<span class="face-value">').text(die.faceValue || 'N/A'),
-        $('<span class="status">').text(die.status || 'Pending'),
-        $('<button>').text('x').click(() => disconnectDice(die.name))
-      );
-      diceContainer.append(diceElement);
-    });
-    updateDiceCount(dice.length);
-  }
-
-  blockDice.addClass('collapsible');
-  diceContainer.addClass('collapsible-content');
-  // Make the dice list not clickable except for the disconnect button
-  diceContainer.off('click').on('click', event => {
-    if (!$(event.target).is('button')) {
-      event.stopPropagation();
-    }
-  });
-  blockDice.off('click').on('click', () => {
-    diceContainer.toggleClass('show');
-  });
-}
-
-function updateDice(diceName, faceValue, status) {
-  const diceElement = $(`.dice[data-name="${diceName}"]`);
-  if (diceElement.length) {
-    diceElement.find('.face-value').text(faceValue || 'N/A');
-    diceElement.find('.status').text(status || 'Pending');
-    diceElement.removeClass('excluded needs-roll rolling rolled').addClass(status.toLowerCase());
-  }
-}
-
-function updateDiceCount(count) {
-  const diceCount = $('#diceCount');
-  if (count === 0) {
-    diceCount.text('No dice connected');
-  } else {
-    diceCount.text(`${count} dice connected`);
-  }
-}
-
-function disconnectDice(name) {
-  sendMessage({ action: 'disconnectDice', name });
-}
-
-function initSelectFromStorage(storageName, defaultValue) {
-  chrome.storage.sync.get(storageName, data => {
-    let value = data[storageName] || defaultValue;
-    selectMessageType.val(value);
-  });
-}
-
-async function fetchJSONMessageTypes() {
-  try {
-    const response = await fetch('messageTypes/index.json');
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const rollFiles = await response.json();
-    const jsonMessageTypes = [];
-
-    for (const file of rollFiles) {
-      const response = await fetch(`messageTypes/${file}`);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const json = await response.json();
-      jsonMessageTypes.push(Message.fromJSON(json.message));
-    }
-    console.log('Roll types:', jsonMessageTypes);
-    return jsonMessageTypes;
-  } catch (error) {
-    console.error('Error fetching roll types:', error);
-    return [];
-  }
-}
-
-function toCamelCase(str) {
-  return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, (match, index) =>
-    index === 0 ? match.toLowerCase() : match.toUpperCase()
-  ).replace(/\s+/g, '');
-}
-
-function saveCustomMessage(name, formula, advDisadvantage, sumRolls) {
-  if (jsonMessageTypes.find(message => message.name === name)) {
-    alert('A pre-defined message with this name already exists. Please choose a different name to store as a custom message.');
-    return -1;
-  } 
-  chrome.storage.sync.get('customMessages', data => {
-    let customMessages = data.customMessages || [];
-    const existingMessageIndex = customMessages.findIndex(msg => msg.name === name);
-    
-    if (existingMessageIndex !== -1) {
-      if (confirm('A custom message with this name already exists. Do you want to overwrite it?')) {
-        customMessages[existingMessageIndex] = new Message(name, formula, advDisadvantage, sumRolls, 'storage').toStorage();
-      }
-    } else {
-      customMessages.push(new Message(name, formula, advDisadvantage, sumRolls, 'storage').toStorage());
-    }
-    chrome.storage.sync.set({ customMessages }, () => console.log('Custom message stored: ' + name));
-  });
-  return;
-}
-
-function saveFormulaToStorage(formula) {
-  chrome.storage.sync.set({ formula }, () => console.log('Formula stored: ' + formula));
-}
-
-function saveEdits(name, formula, advDisadvantage, sumRolls) {
-  chrome.storage.sync.get('customMessages', data => {
-    let customMessages = data.customMessages || [];
-    const index = customMessages.findIndex(msg => msg.name === name);
-    if (index !== -1) {
-      customMessages[index] = new Message(name, formula, advDisadvantage, sumRolls, 'storage').toStorage();
-      chrome.storage.sync.set({ customMessages }, () => console.log('Custom message updated: ' + name));
-    }
-  });
-}
-
-async function populateMessageTypeSelect() {
-  jsonMessageTypes = await fetchJSONMessageTypes();
-  jsonMessageTypes.forEach(roll => {
-    selectMessageType.append(roll.toOption());
-  });
-
-  chrome.storage.sync.get('customMessages', data => {
-    const customMessages = data.customMessages || [];
-    customMessages.forEach(msg => {
-      const message = Message.fromStorage(msg);
-      selectMessageType.append(message.toOption());
-    });
-
-    if (data.customObject) {
-      const customOption = new Message('Custom', data.customObject.formula, data.customObject.advDisadvantage, data.customObject.sumRolls, 'storage').toOption();
-      selectMessageType.append(customOption);
-      textareaFormula.val(data.customObject.formula);
-      checkboxAdvDisadvantage.prop('checked', data.customObject.advDisadvantage);
-      checkboxSumRolls.prop('checked', data.customObject.sumRolls);
-    } else {
-      const customOption = new Message('Custom', '', false, false, 'storage').toOption();
-      selectMessageType.append(customOption);
-    }
-  });
-}
-
-function revertToCustom() {
-  let selectedOption = selectMessageType.find('option:selected');
-  if (selectMessageType.val() !== 'custom') {
-    textAreaCustomName.val(selectedOption.text());
-    selectMessageType.val('custom');
-    selectedOption = selectMessageType.find('option:selected');
-    updateOption(selectedOption, textareaFormula.val(), checkboxAdvDisadvantage.is(':checked'), checkboxSumRolls.is(':checked'));
-    selectMessageType.change();
-  }
-}
-
-function updateOption(option, formula, advDisadvantage, sumRolls) {
-  option.data('formula', formula);
-  option.data('advdisadvantage', advDisadvantage);
-  option.data('sumrolls', sumRolls);
-}
-
-// Initialize message type select
-populateMessageTypeSelect().then(() => {
-  initSelectFromStorage('messageType', 'custom');
-
-  // Add event listener for message type change
-  selectMessageType.change(() => {
-    const selectedOption = selectMessageType.find('option:selected');
-    const formula = selectedOption.data('formula');
-    const advDisadvantage = selectedOption.data('advdisadvantage');
-    const sumRolls = selectedOption.data('sumrolls');
-    console.log('Selected option:', selectedOption.text(), formula, advDisadvantage, sumRolls);
-    textareaFormula.val(formula);
-    checkboxAdvDisadvantage.prop('checked', advDisadvantage);
-    checkboxSumRolls.prop('checked', sumRolls);
-    sendMessage({ action: "setFormula", formula: formula, advDisadvantage: advDisadvantage, sumRolls: sumRolls });
-  });
-
-  // When the user starts typing in the formula box change the selected option to custom
-  textareaFormula.on('input', () => {
-    revertToCustom();
-  });
-
-  // Save checkbox changes to custom object
-  $('#advDisadvantage, #sumRolls').change(() => {
-    sendMessage({ action: "setChecked", advDisadvantage: checkboxAdvDisadvantage.is(':checked'), sumRolls: checkboxSumRolls.is(':checked') });
-  });
-
-});
-
-// Send message to injected JS
+/*
+ * Generic function to send messages to the content script (/js/roll20.js).
+ * This is used by all functions that need to communicate with the content script.
+ */
 function sendMessage(data, responseCallback) {
-  console.log('Sending message to injected JS:', data);
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs =>
-    chrome.tabs.sendMessage(tabs[0].id, data, responseCallback));
+    chrome.tabs.query(
+        { active: true, currentWindow: true },
+        tabs => chrome.tabs.sendMessage(tabs[0].id, data, responseCallback)
+    );
 }
 
-// Hooks "connect" and "disconnect" buttons to injected JS
+/* 
+ * Function to send message requesting the Bluetooth connection window.
+ * This is used when the user clicks the "Connect" button in the popup.
+ */
+function connectDie() {
+    sendMessage({ action: 'connect' });
+    logger('Showing connection window');
+}
 
+/* 
+ * Function to send message requesting a specific die be disconnected.
+ * This is used when the user clicks the "X" button next to a specific Pixel in the popup.
+ */
+function disconnectDie(name) {
+    sendMessage({ action: 'disconnectDie', name });
+    logger(`Disconnected die: ${name}`);
+}
 
-// Gets Roll20 formula from storage
-let selectMessageType = $('#messageType');
-let textareaFormula = $('#formula');
-let textAreaCustomName = $('#customName');
-let checkboxAdvDisadvantage = $('#advDisadvantage');
-let checkboxSumRolls = $('#sumRolls');
-let jsonMessageTypes = [];
+/* 
+ * Function to send message requesting all dice be disconnected.
+ * This is used when the user clicks the "Disconnect" button in the popup.
+ */
+function disconnectAll() {
+    sendMessage({ action: 'disconnectAll' });
+    logger('Disconnected all dice');
+}
 
-// Hook button that saves formula edited in popup
-hookButton('connect');
-hookButton('disconnect');
+/* 
+ * Function to send message with updated status for a specific die.
+ * This is used when the user checks/unchecks a specific Pixel in the popup.
+ */
+function updateDiceStatus(name, status) {
+    sendMessage({ action: 'updateDieStatus', name, status });
+}
 
-let button = $('#save');
-button.click(() => {
-  const formula = textareaFormula.val();
-  const customName = textAreaCustomName.val();
-  const advDisadvantage = checkboxAdvDisadvantage.is(':checked');
-  const sumRolls = checkboxSumRolls.is(':checked');
+/* 
+ * Function to create the base user interface for the popup window.
+ * This runs whenever the popup opens and does not populate the dice list.
+ */
+function createUI(validUrl) {
+    const container = document.getElementById('container');
 
-  if (customName && formula) {
-    if(saveCustomMessage(customName, formula, advDisadvantage, sumRolls) !== -1) {
-      const existingOption = selectMessageType.find(`option:contains(${customName})`);
-      console.log('Existing option:', existingOption);
+    if (validUrl) {
+        const title = document.createElement('h1');
+        title.id = 'title';
+        title.className = 'title';
+        title.innerHTML = 'Roll20Pixels';
+        container.appendChild(title);
 
-      if (existingOption.length) {
-        updateOption(existingOption, formula, advDisadvantage, sumRolls);
-      } else {
-        const option = new Message(customName, formula, advDisadvantage, sumRolls, 'storage').toOption();
-        selectMessageType.append(option);
-      }
+        const connectDiceBtn = document.createElement('button');
+        connectDiceBtn.id = 'connectDice';
+        connectDiceBtn.className = 'connectedDice';
+        connectDiceBtn.innerHTML = '<span>&#10133;</span>Connect';
+        connectDiceBtn.addEventListener('click', () => connectDie());
+        container.appendChild(connectDiceBtn);
+
+        const disconnectDiceBtn = document.createElement('button');
+        disconnectDiceBtn.id = 'disconnectDice';
+        disconnectDiceBtn.className = 'connectedDice';
+        disconnectDiceBtn.innerHTML = '<span>&#10060;</span>Disconnect';
+        disconnectDiceBtn.addEventListener('click', () => disconnectAll());
+        container.appendChild(disconnectDiceBtn);
+
+        const blockDice = document.createElement('div');
+        blockDice.id = 'blockdice';
+        blockDice.className = 'blockdice collapsible';
+        container.appendChild(blockDice);
+
+        const diceCount = document.createElement('div');
+        diceCount.id = 'dicecount';
+        blockDice.appendChild(diceCount);
+
+        const diceList = document.createElement('div');
+        diceList.id = 'diceList';
+        diceList.className = 'collapsible-content show';
+        blockDice.appendChild(diceList);
     }
-  }
+    else {
+        const errorMsg = document.createElement('div');
+        errorMsg.className = 'errorMsgLbl';
+        errorMsg.innerHTML = 'This extension only works on roll20.net';
+        container.appendChild(errorMsg);
+    }
+}
 
-  sendMessage({ action: "setFormula", formula: formula, advDisadvantage: advDisadvantage, sumRolls: sumRolls });
+/* 
+ * Function to create the dice list in the popup.
+ * This runs when the dice list is created based on a message from the content script.
+ */
+function showDice(dice) {
+    const diceContainer = document.getElementById('diceList');
+    const blockDice = document.getElementById('blockdice');
+
+    diceContainer.innerHTML = '';
+    if (dice.length > 0) {
+        dice.forEach(die => {
+            logger(`Creating new die: ${die.name} (${die.status})`);
+            const diceElement = document.createElement('div');
+            const check = document.createElement('input');
+            check.type = 'checkbox';
+            check.id = 'check-' + die.name;
+            if (die.status == 'disabled') { check.checked = false; }
+            else { check.checked = true; }
+            check.addEventListener('click', () => {
+                if (check.checked) { die.status = 'ready'; } 
+                else { die.status = 'disabled'; }
+                updateDiceStatus(die.name, die.status);
+                logger(`Toggling die: ${die.name} (${die.status})`);
+            });
+            diceElement.className = `dice ${die.status}`;
+            diceElement.setAttribute("data-name", die.name);
+            const spanA = document.createElement('span');
+            spanA.textContent = die.name
+            const spanB = document.createElement('span');
+            spanB.className = 'face-value';
+            spanB.textContent = die.faceValue || 'N/A';
+            const btn = document.createElement('button');
+            btn.className = 'connectedDiceSmall';
+            btn.innerHTML = '&#10060;';
+            btn.addEventListener('click', () => disconnectDie(die.name));
+            diceElement.append(check, spanA, spanB, btn);
+            diceContainer.append(diceElement);
+        });
+    }
+
+    diceContainer.removeEventListener('click', function (event) { }, false);
+    diceContainer.addEventListener('click', event => {
+        const eventId = event.target.nodeName;
+        if (!eventId == 'BUTTON') {
+            event.stopPropagation();
+        }
+    }, false);
+
+    blockDice.removeEventListener('click', function (event) { }, false);
+    blockDice.addEventListener('click', () => {
+        diceContainer.classList.toggle('show');
+    });
+}
+
+/* 
+ * Function to update the status of dice in the popup window.
+ * This runs whenever the dice list needs to be updated based on messages from the content script.
+ */
+function updateDice(diceName, faceValue, status) {
+    logger(`Updating die: ${diceName} (${status}) => ${faceValue}`);
+    const diceElement = document.querySelector(`[data-name="${diceName}"]`);
+    if (diceElement == null) {
+        sendMessage({ action: "getStatus" });
+        return;
+    }
+    diceElement.getElementsByClassName('face-value')[0].textContent = faceValue || 'N/A';
+    diceElement.classList.remove("disabled", "needs-roll", "rolling", "rolled", "forced");
+    diceElement.classList.add(status.toLowerCase());
+}
+
+/* 
+ * Create listener for messages sent by the content script.
+ * This runs whenever the content script sends a message to the popup.
+ */
+chrome.runtime.onMessage.addListener((request, _, __) => {
+    if (request.action == "showDice") {
+        showDice(request.dice);
+    } else if (request.action == "updateDiceData") {
+        updateDice(request.diceName, request.faceValue, request.status);
+    }
 });
 
-// Listen on messages from injected JS
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action == "showDice") {
-    console.log('Received message from injected JS:', request.dice);
-    showDice(request.dice);
-  } else if (request.action == "updateDiceData") {
-    console.log('Received updateDiceData message from injected JS:', request);
-    updateDice(request.diceName, request.faceValue, request.status);
-  }
-  // } else if (request.action == "showRollAlert") {
-  //   alert(request.message);
-  // }
-});
-
-// Inject code in website
-// We need to be running in the webpage context to have access to the bluetooth stack
+/* 
+ * Verify if the current tab is a valid Roll20 page.
+ * If it is, inject the content script and create the UI.
+ */
 chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-  chrome.tabs.executeScript(
-    tabs[0].id,
-    { file: "roll20.js" },
-    _ => {
-      sendMessage({ action: "getStatus" });
-      // Always send the current formula displayed in the text box
-      sendMessage({ action: "setFormula", formula: textareaFormula.val() });
-    })
+    let validUrl = false;
+    if (tabs[0].url.includes("roll20.net")) {
+        validUrl = true;
+        chrome.scripting.executeScript({ target: { tabId: tabs[0].id }, files: ["/js/roll20.js"] }, _ => { 
+            logger(`Injected script '/js/roll20.js' into tab ${tabs[0].id}.`);
+            sendMessage({ action: "getStatus" });
+        }); 
+    }
+    createUI(validUrl);
 });

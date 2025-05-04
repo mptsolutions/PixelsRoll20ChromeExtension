30e6891a29581ab8c7fd1debc89e57133031f0b7
mptsolutions <miketurner4@gmail.com>
2025-05-04 14:39:05 -0400

    rewrite of background.js

--- js/background.js
+++ js/background.js
@@ -1,16 +1,25 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function () {
-  // Storage example
-  //chrome.storage.sync.set({color: '#3aa757'}, function() { console.log("The color is green."); });
+/* 
+ * Roll20Pixels Chrome Extension
+ * This file contains the code for the background worker.
+ */
 
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'app.roll20.net' }
-      })
-      ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+
+/* 
+ * Create listener for when the extension is installed.
+ * In theory this will set up the rules for when the extension icon should be shown.
+ * But it is not working as expected, so the icon is always shown.
+ */
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.declarativeContent.onPageChanged.removeRules(async () => {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: { hostSuffix: 'app.roll20.net', schemes: ['https'] },
+                }),
+            ],
+            actions: [ new chrome.declarativeContent.ShowAction()]
+        }]);
+    });
 });
